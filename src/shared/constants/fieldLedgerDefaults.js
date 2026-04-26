8fd0ee9f48c598ce142870c2689b443f2f71a7f1
Tim Rush <timbone72@gmail.com>
2026-04-26 08:48:14 -0500

    Add FieldLedger shared constants

---
+++ src/shared/constants/fieldLedgerDefaults.js
@@ -0,0 +1,22 @@
+export const DEFAULT_HOURLY_RATE = 28;
+
+export const DEFAULT_SELF_EMPLOYMENT_TAX_RATE = 0.153;
+
+export const DEFAULT_FEDERAL_TAX_RATE = 0.12;
+
+export const JOB_TYPES = {
+  BUCKING: "bucking",
+  TORQUE_TURN: "torque_turn",
+};
+
+export const EXPENSE_CATEGORIES = [
+  "Fuel",
+  "Food",
+  "Tools",
+  "Lodging",
+  "Supplies",
+  "Other",
+];
+
+export const TAX_DISCLAIMER =
+  "Tax estimates are for planning only and are not tax advice.";
