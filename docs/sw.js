53983827628d4a6513e1b5fd717e7f6f5c8c13ed
Tim Rush <timbone72@gmail.com>
2026-05-07 17:53:16 -0500

    Add PWA install foundation

---
+++ docs/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "fieldledger-v1";
+
+const APP_SHELL = [
+  "/fieldledger/",
+  "/fieldledger/manifest.webmanifest",
+  "/fieldledger/favicon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_SHELL);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(event.request).catch(() => {
+          return caches.match("/fieldledger/");
+        })
+      );
+    })
+  );
+});
